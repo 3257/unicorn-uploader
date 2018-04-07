19d267c8e96f3a8723be5ac7a326764526cd311c
Deyan Aleksandrov <deyanaaleksandrov@gmail.com>
2018-04-07 18:15:41 +0300

    UploadUnicornVC ready

--- unicornUploader/ViewControllers/UploadUnicornViewController.swift
+++ unicornUploader/ViewControllers/UploadUnicornViewController.swift
@@ -20,10 +20,12 @@ class UploadUnicornViewController: UIViewController {
     @IBOutlet weak var submitButton: UIButton!
 
     @IBAction func didTapSubmit(_ sender: UIButton) {
+        // Get properties for the unicorn-to-be-created
         let addedBy = self.addedBy.text ?? ""
         let seenAt = self.seenAt.text ?? ""
         let unicorn = Unicorn(imagePath: storageImagePath, addedBy: addedBy, seenAt: seenAt)
 
+        // Create the unicorn and record it
         writeUnicornToDatabase(unicorn)
 
         // Return to Unicorns Table VC
@@ -56,14 +58,15 @@ class UploadUnicornViewController: UIViewController {
 
     // MARK: - Functions
     fileprivate func writeUnicornToDatabase(_ unicorn: Unicorn) {
+        // Access the "unicorns" child reference and then access (create) a unique child reference within it and finally set its value
         ref.child("unicorns").child(unicorn.seenAt + "\(Int(Date.timeIntervalSinceReferenceDate * 1000))").setValue(unicorn.toAnyObject())
     }
 
-    fileprivate func uploadSuccess(_ metadata: StorageMetadata, _ storagePath: String, _ storageImage: UIImage) {
+    fileprivate func uploadSuccess(_ storagePath: String, _ storageImage: UIImage) {
 
-        // Shpw updated image for unicorn
+        // Update the unicorn image view with the selected image
         unicornImageView.image = storageImage
-        // Updated path for image
+        // Updated global variable for the storage path for the selected image
         storageImagePath = storagePath
 
         // Enable submit button and change its color
@@ -100,26 +103,33 @@ extension UploadUnicornViewController: UIImagePickerControllerDelegate, UINaviga
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         picker.dismiss(animated: true, completion: nil)
 
+        // 1. Get image data from selected image
         guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage,
             let imageData = UIImageJPEGRepresentation(image, 0.5) else {
                 print("Could not get Image JPEG Representation")
                 return
         }
 
+        // 2. Create a unique image path for image. In the case I am using the googleAppId of my account appended to the interval between 00:00:00 UTC on 1 January 2001 and the current date and time as an Integer and then I append .jpg. You can use whatever you prefer as long as it ends up unique.
         let imagePath = Auth.auth().app!.options.googleAppID + "/\(Int(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
 
+        // 3. Set up metadata with appropriate content type
         let metadata = StorageMetadata()
         metadata.contentType = "image/jpeg"
 
+        // 4. Show activity indicator
         showNetworkActivityIndicator = true
 
-        storageUploadTask = self.storageRef.child(imagePath).putData(imageData, metadata: metadata) { (metadata, error) in
+        // 5. Start upload task
+        storageUploadTask = storageRef.child(imagePath).putData(imageData, metadata: metadata) { (_, error) in
+            // 6. Hide activity indicator because uploading is done with or without an error
             self.showNetworkActivityIndicator = false
+
             guard error == nil else {
                 print("Error uploading: \(error!)")
                 return
             }
-            self.uploadSuccess(metadata!, imagePath, image)
+            self.uploadSuccess(imagePath, image)
         }
     }
 
